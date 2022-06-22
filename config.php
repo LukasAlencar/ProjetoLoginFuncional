d7c317a198c927e47d2bf3e6ae8ba7a71617201f
Lucas Kauan <lucas.kauan@aluno.ifsp.edu.br>
2022-06-22 15:06:17 -0300

    ProjetoFuncional

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'projetologin'
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+     if($conexao->connect_errno){
+        echo "Error";
+     }
+     else{
+        echo "Conexão efetuada com sucesso";
+     }
+
+?>
